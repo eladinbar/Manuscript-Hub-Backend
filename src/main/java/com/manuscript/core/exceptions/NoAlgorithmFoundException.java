86bccb6a264c668eccb06c72c37c4dce8a325574
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-03-30 12:38:14 +0300

    Added and fixed some exceptions.

--- src/main/java/com/manuscript/core/exceptions/NoAlgorithmFoundException.java
+++ src/main/java/com/manuscript/core/exceptions/NoAlgorithmFoundException.java
@@ -1,7 +1,7 @@
 package com.manuscript.core.exceptions;
 
 public class NoAlgorithmFoundException extends RuntimeException {
-    public static final String message = "No user found in DB";
+    public static final String message = "No algorithm found in the database.";
 
     public NoAlgorithmFoundException() {
         super(message);
