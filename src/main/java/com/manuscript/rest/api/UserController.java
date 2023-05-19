38dcd0c461b9d4af9d445b9b88ce38812f3f8893
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-05-19 23:50:23 +0300

    Fixed UserController update function, getUserById function and added a getUserByUid function as well.

--- src/main/java/com/manuscript/rest/api/UserController.java
+++ src/main/java/com/manuscript/rest/api/UserController.java
@@ -1,24 +1,17 @@
 package com.manuscript.rest.api;
 
-import com.google.firebase.auth.FirebaseAuthException;
-import com.manuscript.core.domain.common.enums.Role;
-import com.manuscript.core.domain.user.models.UserModel;
-import com.manuscript.infrastructure.firebase.security.Roles.RoleConstants;
 import com.manuscript.rest.request.UserRequest;
-import com.manuscript.rest.response.ImageResponse;
 import com.manuscript.rest.response.UserResponse;
 import com.manuscript.rest.service.IUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.management.relation.RoleNotFoundException;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Stream;
 
 import static com.manuscript.rest.common.Constants.RESOURCE_USER;
-import static lombok.Lombok.checkNotNull;
 
 @RestController
 @RequestMapping(RESOURCE_USER)
@@ -29,15 +22,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "/getUserDetails/{id}", method = RequestMethod.GET)
-    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id)  {
-        if (id == null){ throw new IllegalArgumentException(); }
-        UserResponse result = userService.getById(id);
-        return ResponseEntity.ok(result);
-    }
-
-    @PatchMapping("/updateUser/{id}")
-    public ResponseEntity<UserResponse> update(@RequestBody UserRequest user, @PathVariable UUID id) {
+    @PatchMapping("/updateUser")
+    public ResponseEntity<UserResponse> update(@RequestBody UserRequest user) {
         checkNotNull(user, true);
         try {
             return ResponseEntity.ok(this.userService.updateUser(user));
@@ -45,6 +31,20 @@ public class UserController {
             System.err.println("Error updating this user: " + e.getMessage());
         }
         return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(null);
+    }
+
+    @GetMapping(value = "/getUserById/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id)  {
+        if (id == null){ throw new IllegalArgumentException(); }
+        UserResponse result = userService.getById(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/getUserByUid/{uid}")
+    public ResponseEntity<UserResponse> getUserByUid(@PathVariable String uid)  {
+        if (uid == null){ throw new IllegalArgumentException(); }
+        UserResponse result = userService.getByUid(uid);
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/deleteUser/{id}")
@@ -58,5 +58,4 @@ public class UserController {
                 userRequest.getEmail(), userRequest.getName(), userRequest.getPhoneNumber()).anyMatch(Objects::isNull))
             throw new IllegalArgumentException("User request's fields must not be null.");
     }
-
 }
