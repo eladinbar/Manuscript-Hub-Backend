6896cf00ad4cad31b9dee53e47705794b136666b
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-06-05 20:28:45 +0300

    Updated UserControllerTests.java to the current UserController.java signatures. Made some minor cosmetic adjustments to UserController.

--- src/main/java/com/manuscript/rest/api/UserController.java
+++ src/main/java/com/manuscript/rest/api/UserController.java
@@ -18,13 +18,14 @@ import static com.manuscript.rest.common.Constants.RESOURCE_USER;
 @CrossOrigin()
 public class UserController {
     private final IUserService userService;
+
     public UserController(IUserService userService) {
         this.userService = userService;
     }
 
     @PatchMapping("/updateUser")
-    public ResponseEntity<UserResponse> update(@RequestBody UserRequest user) {
-        checkNotNull(user, true);
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest user) {
+        checkNotNull(user);
         try {
             return ResponseEntity.ok(this.userService.updateUser(user));
         } catch (Exception e) {
@@ -34,22 +35,25 @@ public class UserController {
     }
 
     @GetMapping(value = "/getUserById/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id)  {
-        if (id == null){ throw new IllegalArgumentException(); }
+    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
+        if (id == null)
+            throw new IllegalArgumentException("Invalid user ID.");
         UserResponse result = userService.getById(id);
         return ResponseEntity.ok(result);
     }
 
     @GetMapping(value = "/getUserByUid/{uid}")
-    public ResponseEntity<UserResponse> getUserByUid(@PathVariable String uid)  {
-        if (uid == null){ throw new IllegalArgumentException(); }
+    public ResponseEntity<UserResponse> getUserByUid(@PathVariable String uid) {
+        if (uid == null)
+            throw new IllegalArgumentException("Invalid firebase user ID.");
         UserResponse result = userService.getByUid(uid);
         return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/deleteUser/{id}")
-    public void deleteUser(@PathVariable UUID id){
-        if (id == null){ throw new IllegalArgumentException(); }
+    public void deleteUserById(@PathVariable UUID id) {
+        if (id == null)
+            throw new IllegalArgumentException("Invalid user ID.");
         userService.deleteUser(id);
     }
 
@@ -58,8 +62,8 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-    private void checkNotNull(UserRequest userRequest, boolean newRequest) {
-        if (Stream.of(userRequest.getUid(),  userRequest.getRole(),
+    private void checkNotNull(UserRequest userRequest) {
+        if (Stream.of(userRequest.getUid(), userRequest.getRole(),
                 userRequest.getEmail(), userRequest.getName(), userRequest.getPhoneNumber()).anyMatch(Objects::isNull))
             throw new IllegalArgumentException("User request's fields must not be null.");
     }
